c5ddd7710791198aef545bdbd411ca35e4355584
CalvinKersten <113361043+CalvinKersten@users.noreply.github.com>
2023-11-27 20:47:47 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace VehicleComparer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
